7a8e6189d00000f1a5a069861d403d925724c0ad
Silviu Ojog <sil.ojog@gmail.com>
2025-02-07 13:40:29 +0200

    Exercitiul numpy

---
+++ Ziua2/exercitiu_numpy.py
@@ -0,0 +1,91 @@
+
+# %%
+######### Exercitiul ################
+# Creați un vector cu 10 elemente întregi, toate fiind egale cu 0.
+
+
+
+
+# %%
+######### Exercitiul ################
+# Creați un vector cu 10 elemente întregi, toate fiind egale cu 1.
+
+
+
+
+# %%
+######### Exercitiul ################
+# Creați un vector cu elemente întregi de la 0 la 9.
+
+
+
+
+# %%
+######### Exercitiul ################
+# Creați un vector cu elemente întregi de la 10 la 29.
+
+
+
+# %%
+######### Exercitiul ################
+# Creați un vector cu elemente întregi de la 0 la 9 în ordine inversă.
+
+
+
+
+# %%
+######### Exercitiul ################
+# Creați un vector cu 10 elemente aleatoare între 0 și 1.
+
+
+
+# %%
+######### Exercitiul ################
+# Creați o matrice 3x3 cu elemente întregi aleatoare între 0 și 9.
+
+
+
+# %%
+######### Exercitiul ################
+# Creați o matrice identitate 5x5.
+
+
+
+
+# %%
+######### Exercitiul ################
+# Adunați două matrici de dimensiuni egale (de exemplu, 3x3).
+
+
+
+# %%
+######### Exercitiul ################
+# Înmulțiți două matrici de dimensiuni corespunzătoare (de  exemplu, 2x3 și 3x2).
+
+
+
+
+# %%
+######### Exercitiul ################
+# Transpuneți o matrice (de exemplu, 3x4).
+
+
+
+# %%
+######### Exercitiul ################
+# Găsiți valoarea maximă dintr-un vector.
+
+
+
+# %%
+######### Exercitiul ################
+# Găsiți indicele valorii maxime dintr-un vector.
+
+
+
+# %%
+######### Exercitiul ################
+# Găsiți media aritmetică a valorilor dintr-o matrice.
+
+
+# %%
